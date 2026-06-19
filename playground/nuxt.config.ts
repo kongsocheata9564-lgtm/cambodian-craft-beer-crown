c7b1ac195d10983d66a52f5879c2adb7b0c996b1
haksovanphanha99-dev <haksovanphanha99@gmail.com>
2026-06-19 13:13:24 +0700

    my page is ready

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: 'latest',
   myModule: {},
+   app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+        }
+      ],
+    }
+  }
 })
