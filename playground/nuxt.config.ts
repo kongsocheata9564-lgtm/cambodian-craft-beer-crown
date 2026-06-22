db596d6aac9b0ff955fc3154dea967f587480c78
theasithul <theasithul9@gmail.com>
2026-06-22 09:58:39 +0700

    completed in page award

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -3,4 +3,21 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   compatibilityDate: 'latest',
   myModule: {},
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css'
+        }
+      ],
+
+      script: [
+        {
+          src: 'https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4',
+          type: 'module'
+        }
+      ]
+    }
+  }
 })
