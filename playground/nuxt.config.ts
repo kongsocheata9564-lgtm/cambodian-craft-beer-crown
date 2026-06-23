2d24df71a5350d0b728bf3a02d6dff050aaf4353
Rith0068 <sothearithkoem@gmail.com>
2026-06-23 14:24:03 +0700

    remove unused tailwindcss module

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  modules: ['my-module','@nuxtjs/tailwindcss'],
+  modules: ['my-module'],
   devtools: { enabled: true },
   compatibilityDate: 'latest',
   myModule: {},
